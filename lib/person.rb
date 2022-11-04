3763126921cf0ca68ddb5739cc1ee24f46751288
Bo Brockmann <bbrockmann1@gmail.com>
2022-11-03 19:05:35 -0600

    commit tree 6e759569ac24d020ebad6ae291230a8573c0b20d

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
-# your code goes here
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness :hygiene
+
+    def initialize name, bank_account, happiness, hygiene
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness num
+       @happiness =  if num > 10
+                        10
+                     elsif num < 0
+                         0
+                     else
+                        num
+                    end
+    
+end
